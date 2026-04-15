c0925932e724a0bdd0406e2e9ea1d04ffdfdcae0
GabrielColdebella <gabriel.henrique@mail.uft.edu.br>
2026-04-14 21:13:42 -0300

    Alteração pequena no arquivo de conexão
    correção do comando connecterrno

--- config/conexao.php
+++ config/conexao.php
@@ -6,7 +6,7 @@
 
     $mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
     if ($mysqli->connect_errno){
-        echo "falha:(" . $mysqli->connect_errno . ")". $mysqli->connect_error;
+        echo "falha:(" . $mysqli->connect_errno . ")". $mysqli->connecterrno;
     }
     else {
         // echo "Conectado ao banco de dados";
